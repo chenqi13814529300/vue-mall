13bdbc09ee5fcfa9f097930470821d29c40dedb8
maplechen <1146706468@qq.com>
2022-03-04 09:54:11 +0800

    品牌懒加载和屏幕滚动实现

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import API from './api'
-
+Vue.prototype.$bus = new Vue()
 Vue.config.productionTip = false
 Vue.prototype.$API = API
 
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,26 +4,27 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
-    <home-swiper :banner="banner"></home-swiper>
-    <recommend-view :recommend="recommend"></recommend-view>
-    <feature-view></feature-view>
-    <tab-control
-      :titles="['流行', '新款', '精选']"
-      @tabClick="tabClick"
-      ref="tabControl2"
-    />
-    <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-    </ul>
+    <my-scroll
+      class="content"
+      ref="scroll"
+      @scroll="contentScroll"
+      :pull-up-load="true"
+      @pullingUp="loadMore"
+      :probe-type="3"
+    >
+      <home-swiper :banner="banner"></home-swiper>
+      <recommend-view :recommend="recommend"></recommend-view>
+      <feature-view></feature-view>
+      <tab-control
+        class="tab-control"
+        :titles="['流行', '新款', '精选']"
+        @tabClick="tabClick"
+        ref="tabControl2"
+        v-show="isTabFixed"
+      />
+      <goods-list :goods="goods[currentType].list"></goods-list>
+    </my-scroll>
+    <back-top @click.native="backClick" v-show="isShowBackTop" />
   </div>
 </template>
 
@@ -34,15 +35,39 @@ import NavBar from "components/common/navbar/NavBar";
 import HomeSwiper from "./childComps/HomeSwiper.vue";
 import RecommendView from "./childComps/RecommendView.vue";
 import FeatureView from "./childComps/FeatureView.vue";
-import TabControl from 'components/content/tabControl/TabControl'
+import TabControl from "components/content/tabControl/TabControl";
+import GoodsList from "components/content/goods/GoodsList.vue";
+import myScroll from "components/common/scroll/Scroll.vue";
+import BackTop from "components/content/backTop/BackTop";
+
+  import {debounce} from "common/utils";
 export default {
   //import引入的组件需要注入到对象中才能使用
-  components: { NavBar, HomeSwiper, RecommendView, FeatureView,TabControl },
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    TabControl,
+    GoodsList,
+    myScroll,
+    BackTop,
+  },
   data() {
     //这里存放数据
     return {
       banner: [],
       recommend: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] },
+      },
+      currentType: "pop",
+      isShowBackTop: false,
+      tabOffsetTop: 0,
+      isTabFixed: false,
+      saveY: 0,
     };
   },
   //监听属性 类似于data概念
@@ -50,23 +75,72 @@ export default {
   //监控data中的数据变化
   watch: {},
   //方法集合
-  methods: {},
+  methods: {
+    // swiperImageLoad() {
+    //   this.tabOffsetTop = this.$refs.tabControl2.$el.offsetTop;
+    // },
+    backClick() {
+      console.log(22);
+      this.$refs.scroll.scrollTo(0, 0);
+    },
+    contentScroll(position) {
+      // 1.判断BackTop是否显示
+      this.isShowBackTop = -position.y > 1000;
+
+      // 2.决定tabControl是否吸顶(position: fixed)
+      this.isTabFixed = -position.y > this.tabOffsetTop;
+    },
+    getHomeMultidata() {
+      // 请求多个数据
+      this.$API.home.getHomeMultidata().then((res) => {
+        this.banner = res.data.banner.list;
+        this.recommend = res.data.recommend.list;
+      });
+    },
+    getHomeGoods(type) {
+      // 请求商品数据
+      const page = this.goods[type].page + 1;
+      this.$API.home.getHomeGoods(type, page).then((res) => {
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page++;
+        console.log(this.goods[type]);
+        this.$refs.scroll.finishPullUp()
+      });
+    },
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = "pop";
+          break;
+        case 1:
+          this.currentType = "new";
+          break;
+        case 2:
+          this.currentType = "sell";
+          break;
+      }
+    },
+    loadMore() {
+        console.log("再来");
+
+      this.getHomeGoods(this.currentType);
+    },
+  },
   //生命周期 - 创建完成（可以访问当前this实例）
   created() {
-    // 请求多个数据
-    this.$API.home.getHomeMultidata().then((res) => {
-      this.banner = res.data.banner.list;
-      this.recommend = res.data.recommend.list;
-      console.log(res);
-    });
-
-    //  this.$API.home.getHomeGoods().then((res) => {
-
-    //   console.log(res);
-    // });
+    this.getHomeMultidata();
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
   },
   //生命周期 - 挂载完成（可以访问DOM元素）
-  mounted() {},
+  mounted() {
+    //     // 1.图片加载完成的事件监听
+      const refresh = debounce(this.$refs.scroll.refresh, 50)
+      this.$bus.$on('itemImageLoad', () => {
+        refresh()
+      })
+  },
   beforeCreate() {}, //生命周期 - 创建之前
   beforeMount() {}, //生命周期 - 挂载之前
   beforeUpdate() {}, //生命周期 - 更新之前
@@ -78,8 +152,28 @@ export default {
 </script>
 <style scoped lang="less">
 /*@import url(); 引入公共css类*/
+#home {
+  height: 100vh;
+  position: relative;
+}
+
 .home-nav {
   background-color: var(--color-tint);
-  color: white;
+  color: #fff;
+  /*在使用浏览器原生滚动时, 为了让导航不跟随一起滚动*/
+}
+
+.content {
+  overflow: hidden;
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
+}
+
+.tab-control {
+  position: relative;
+  z-index: 9;
 }
 </style>
